e2dcefc3cd4ae212cbc3da392ce1f6a46f093ef5
mac <mac@mac-MacBook-Air.local>
2023-09-07 16:29:16 +0300

    movie schame create

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = ''; // DB URI
+const MONGO_URL = 'mongodb+srv://samedkarakayali:Veriyaz348484@cluster0.cyfkfac.mongodb.net/'; // DB URI
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
